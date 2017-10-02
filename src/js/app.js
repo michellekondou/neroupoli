7825577bf8c7d6eb7b6045d00c3ab10ae0755dbf
michellekondou <michellekondou@gmail.com>
2017-10-02 23:38:08 +0300

    remove service workers

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
-//service worker registration
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+// //service worker registration
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
 
 var cw = document.documentElement.clientWidth;
 var ch = document.documentElement.clientHeight;
