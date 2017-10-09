ff0b95b11b7839d633ec34c1ba088cc0ff035c0b
michellekondou <michellekondou@gmail.com>
2017-10-09 17:53:53 +0300

    fix paths in sw

--- dist/sw.js
+++ dist/sw.js
@@ -3,7 +3,6 @@ var urlsToCache = [
   'graphics/map-1920x1080-v42.svg',
   'graphics/map-1366x768-v42.svg',
   'graphics/map-768x1024-v42.svg',
-  'proxy/data-52256a609b.json',
   'assets/app.css',
   'assets/app-974aba1d89.js',
   'fonts/cfastystd-bold-webfont.woff2',
