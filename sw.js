fc069345f91bdf268f7d84d47b561174bd386bf8
michellekondou <michellekondou@gmail.com>
2017-10-17 09:16:17 +0300

    add assets

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ var urlsToCache = [
   'dist/graphics/map-1366x768-v42.svg',
   'dist/graphics/map-768x1024-v42.svg',
   'dist/assets/app.css',
-  'dist/assets/app-e96a2fb1bc.js',
+  'dist/assets/app-4915f06d48.js',
   'dist/fonts/cfastystd-bold-webfont.woff2',
   'dist/fonts/cfastystd-book-webfont.woff2',
   'dist/svg/sprite.svg'
